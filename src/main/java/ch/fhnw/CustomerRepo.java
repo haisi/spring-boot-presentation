58776171e3a3b4cb37c207d909413be9e57efa9e
Hasan Kara <hasan.kara@fhnw.ch>
2015-09-01 15:39:11 +0200

    Use RestResource to publish JpaRepository as Rest-Endpoint
    Features include:
    - Filtering
    - Sorting
    - Navigating
    - HAL-format

--- src/main/java/ch/fhnw/CustomerRepo.java
+++ src/main/java/ch/fhnw/CustomerRepo.java
@@ -1,11 +1,14 @@
 package ch.fhnw;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.Collection;
 
+@RestResource
 public interface CustomerRepo extends JpaRepository<Customer, Long> {
 
-    Collection<Customer> findByName(String name);
+    Collection<Customer> findByName(@Param("name") String name);
 
 }
